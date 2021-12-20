1df0658fa97bdc3859691d5d8aa0c688628e89f7
luisalfredosv <luissarabia80@gmail.com>
2021-12-19 20:12:12 -0400

    [update] Import css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import { Provider } from "react-redux";
+import store from "./store";
 
 import { NavBar } from "./layouts/NavBar";
 import { Main } from "./layouts/Main";
-
 import { View } from "./components/View";
-
-import { Provider } from "react-redux";
-
-import store from "./store";
 
 function App() {
 	return (
 		<Provider store={store}>
 			<NavBar />
-			<Main className='justify-content-center'>
+			<Main>
 				<View />
 			</Main>
 		</Provider>
