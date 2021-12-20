9b68b3443d975df472ccbc84f2d82967bd38d3f8
luisalfredosv <luissarabia80@gmail.com>
2021-12-19 20:11:53 -0400

    [update] Add catch error

--- src/actions/responsesActions.js
+++ src/actions/responsesActions.js
@@ -3,12 +3,16 @@ import axios from "axios";
 import { ADD_RESPONSE } from "./types";
 
 export const send = (text) => async (dispatch) => {
-	const { data } = await axios.get(
-		`http://localhost:8080/iecho?text=${text}`
-	);
+	try {
+		const { data } = await axios.get(
+			`http://localhost:8080/iecho?text=${text}`
+		);
 
-	dispatch({
-		type: ADD_RESPONSE,
-		payload: data,
-	});
+		dispatch({
+			type: ADD_RESPONSE,
+			payload: data,
+		});
+	} catch (e) {
+		console.log(e);
+	}
 };
