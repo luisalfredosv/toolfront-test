99ed935459996bf3013913bb56f02457eaa6462b
luisalfredosv <luissarabia80@gmail.com>
2021-12-20 09:30:33 -0400

    [config] Add URL backend

--- src/actions/responsesActions.js
+++ src/actions/responsesActions.js
@@ -3,16 +3,13 @@ import axios from "axios";
 import { ADD_RESPONSE } from "./types";
 
 export const send = (text) => async (dispatch) => {
-	try {
-		const { data } = await axios.get(
-			`http://localhost:8080/iecho?text=${text}`
-		);
+	const URL_BACK = "https://tranquil-taiga-63901.herokuapp.com";
+	// OR http://localhost:8080 in local
 
-		dispatch({
-			type: ADD_RESPONSE,
-			payload: data,
-		});
-	} catch (e) {
-		console.log(e);
-	}
+	const { data } = await axios.get(`${URL_BACK}/iecho?text=${text}`);
+
+	dispatch({
+		type: ADD_RESPONSE,
+		payload: data,
+	});
 };
